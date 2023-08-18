da7984543f5253fcc32b580013971f37c1c0ca7e
hutamatr <hutamatr@gmail.com>
2023-08-18 16:43:16 +0700

    feat: add refresh token to register

--- src/extensions/users-permissions/controllers/validation/auth.js
+++ src/extensions/users-permissions/controllers/validation/auth.js
@@ -10,15 +10,13 @@ const callbackBodySchema = yup.object().shape({
 const registerBodySchema = yup.object().shape({
   email: yup.string().email().required(),
   username: yup.string().min(3).required(),
-  password: yup
-    .string()
-    .required()
-    .matches(
-      /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
-      "Password must be min 8 characters, and have 1 Special Character, 1 Uppercase, 1 Number and 1 Lowercase"
-    ),
-  firstname: yup.string().required().min(3),
-  lastname: yup.string().required().min(3).max(30),
+  password: yup.string().required(),
+  // .matches(
+  //   /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+  //   "Password must be min 8 characters, and have 1 Special Character, 1 Uppercase, 1 Number and 1 Lowercase"
+  // ),
+  // firstname: yup.string().required().min(3),
+  // lastname: yup.string().required().min(3).max(30),
 });
 
 const sendEmailConfirmationBodySchema = yup.object().shape({
